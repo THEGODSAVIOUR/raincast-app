8bd2d7fb8820042c38cf5cfa2de983f1e6a43c29
Eshan Srivastava <eshansri28@gmail.com>
2025-11-05 20:37:12 +0530

    feat: Add animated raindrop background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import React from "react";
 import "./App.css";
 import RainfallForm from "./components/RainfallForm";
@@ -5,7 +6,15 @@ import RainfallForm from "./components/RainfallForm";
 function App() {
   return (
     <div className="App">
-      <RainfallForm />
+      {/* This new container will hold the animated raindrops */}
+      <div className="background-container">
+        <div className="rain"></div>
+      </div>
+
+      {/* This container ensures your form stays on top of the background */}
+      <div className="content-container">
+        <RainfallForm />
+      </div>
     </div>
   );
 }
