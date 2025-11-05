d657340555b61b31df1fe785df115f8cf9273aa5
Eshan Srivastava <eshansri28@gmail.com>
2025-11-05 22:52:32 +0530

    feat: Implement dynamic stretching rain animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,12 @@ import "./App.css";
 import RainfallForm from "./components/RainfallForm";
 
 function App() {
-  // Create an array of 50 elements to map into raindrop spans
-  const raindrops = Array.from({ length: 50 });
+  // Create an array of 80 elements to map into raindrop spans for a fuller effect
+  const raindrops = Array.from({ length: 80 });
 
   return (
     <div className="App">
       <div className="background-container">
-        {/* The container for all our raindrop elements */}
         <div className="rain">
           {raindrops.map((_, idx) => (
             <span key={idx} className="raindrop"></span>
